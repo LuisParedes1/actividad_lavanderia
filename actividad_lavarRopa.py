09e192c2450e6e2744d72ddfd42930eb20ca8d4d
Luis Paredes <luisjosepr@gmail.com>
2022-08-24 09:22:18 -0300

    Estado inicial del ejercicio
    Se lee el archivo y se crea el archivo final a entregar

---
+++ actividad_lavarRopa.py
@@ -0,0 +1,17 @@
+datos = open('/home/luis/Documents/UBA_Drive/UBA/C5/modelos_optimizacion/actividades/semana_1/primer_problema.txt', 'r')
+archivo_final = open('/home/luis/Documents/UBA_Drive/UBA/C5/modelos_optimizacion/actividades/semana_1/primer_solucion.txt', 'w')
+
+for linea_leida in datos:
+
+    linea = linea_leida.split()
+
+    # Leo el problema
+    if 'p' == linea[0]:
+        
+        print(linea)
+
+    
+
+
+datos.close()
+archivo_final.close()
